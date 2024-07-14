0702c674dcb21cee77a30b1402e1d7c9419ff972
qaq <1220062330@qq.com>
2024-07-14 22:55:44 +0800

    update

--- 1.py
+++ 1.py
@@ -8,6 +8,12 @@ from sklearn.metrics import classification_report, confusion_matrix
 from typing import *
 from numpy.linalg import *
 
+# 设置文件的读写权限
+# os.chmod('/data/MNIST/train-images-idx3-ubyte', 0o777)
+# os.chmod('./data/MNIST/train-labels-idx1-ubyte', 0o777)
+# os.chmod('./data/MNIST/t10k-images-idx3-ubyte', 0o777)
+# os.chmod('./data/MNIST/t10k-labels-idx1-ubyte', 0o777)
+
 train_image_file = './data/MNIST/raw/train-images-idx3-ubyte'
 train_label_file = './data/MNIST/raw/train-labels-idx1-ubyte'
 test_image_file = './data/MNIST/raw/t10k-images-idx3-ubyte'
@@ -366,4 +372,196 @@ labels = np.argmax(testY, axis=1)
 # print(confusion_matrix(predictions, labels))
 print(classification_report(predictions, labels))
 
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# design a neural network class
+class NeuralNetwork():
+    """
+    Fully connected neural network.
+    Attributes:
+      n_layers is the number of layers.
+      activation is a list of Activation objects corresponding to each layer's activation function.
+      loss is a Loss object corresponding to the loss function used to train the network.
+      learning_rate is the learning rate.
+      W is a list of weight matrix used in each layer.
+      b is a list of biases used in each layer.
+    """
+
+    def __init__(self, layer_size: List[int], activation: List[Activation], loss: Loss,
+                 learning_rate: float = 0.01) -> None:
+        """
+        Initializes a NeuralNetwork object
+        """
+        assert len(activation) == len(layer_size), \
+            "Number of sizes for layers provided does not equal the number of activation"
+        self.layer_size = layer_size
+        self.num_layer = len(layer_size)
+        self.activation = activation
+        self.loss = loss
+        self.learning_rate = learning_rate
+        self.W = []
+        self.b = []
+        for i in range(self.num_layer - 1):
+            W = np.random.randn(layer_size[i], layer_size[i + 1])
+            b = np.random.randn(1, layer_size[i + 1])
+            self.W.append(W)
+            self.b.append(b)
+        self.A = []
+        self.Z = []
+
+    def forward(self, X: np.ndarray) -> (List[np.ndarray], List[np.ndarray]):
+        """
+        Forward pass of the network on a dataset of n examples with m features. Except the first layer, each layer
+        computes linear transformation plus a bias followed by a nonlinear transformation.
+        Parameters:
+          X is the training data with shape (n, m).
+        Returns:
+          A is a list of numpy data, representing the output of each layer after the first layer. There are
+            self.num_layer numpy arrays in the list and each array is of shape (n, self.layer_size[i]).
+          Z is a list of numpy data, representing the input of each layer after the first layer. There are
+            self.num_layer numpy arrays in the list and each array is of shape (n, self.layer_size[i]).
+        """
+        num_sample = X.shape[0]
+        A, Z = [], []
+        for i in range(self.num_layer):
+            if i == 0:
+                a = X.copy()
+                z = X.copy()
+            else:
+                a = A[-1]
+                z = a.dot(self.W[i - 1]) + self.b[i - 1]
+                a = self.activation[i].value(z)
+            Z.append(z)
+            A.append(a)
+        self.A = A
+        self.Z = Z
+        return Z[-1], A[-1]
+
+    def backward(self, dLdyhat) -> List[np.ndarray]:
+        """
+        Backward pass of the network on a dataset of n examples with m features. The derivatives are computed from
+          the end of the network to the front.
+        Parameters:
+          Z is a list of numpy data, representing the input of each layer. There are self.num_layer numpy arrays in
+            the list and each array is of shape (n, self.layer_size[i]).
+          dLdyhat is the derivative of the empirical loss w.r.t. yhat which is the output of the neural network.
+            dLdyhat is with shape (n, self.layer_size[-1])
+        Returns:
+          dZ is a list of numpy array. Each numpy array in dZ represents the derivative of the emipirical loss function
+            w.r.t. the input of that specific layer. There are self.n_layer arrays in the list and each array is of
+            shape (n, self.layer_size[i])
+        """
+        dZ = []
+        for i in range(self.num_layer - 1, -1, -1):
+            if i == self.num_layer - 1:
+                dLdz = dLdyhat
+            else:
+                dLda = np.dot(dLdz, self.W[i].T)
+                dLdz = self.activation[i].derivative(self.Z[i]) * dLda  # derivative w.r.t. net input for layer i
+            dZ.append(dLdz)
+        dZ = list(reversed(dZ))
+        self.dLdZ = dZ
+        return self.dLdZ
+
+    def update_weights(self) -> List[np.ndarray]:
+        '''
+        Having computed the delta values from the backward pass, update each weight with the sum over the training
+        examples of the gradient of the loss with respect to the weight.
+        :param X: The training set, with size (n, f)
+        :param Z_vals: a list of z-values for each example in the dataset. There are n_layers items in the list and
+                       each item is an array of size (n, layer_sizes[i])
+        :param deltas: A list of delta values for each layer. There are n_layers items in the list and
+                       each item is an array of size (n, layer_sizes[i])
+        :return W: The newly updated weights (i.e. self.W)
+        '''
+        for i in range(self.num_layer - 1):
+            a = self.A[i]
+            dW = np.dot(a.T, self.dLdZ[i + 1])
+            db = np.sum(self.dLdZ[i + 1], axis=0, keepdims=True)
+            self.W[i] -= self.learning_rate * dW
+            self.b[i] -= self.learning_rate * db
+        return self.W
+
+    def one_epoch(self, X: np.ndarray, Y: np.ndarray, batch_size: int, train: bool = True) -> (float, float):
+        """
+        One epoch of either training or testing procedure.
+        Parameters:
+          X is the data input. X is a two-dimensional numpy array.
+          Y is the data label. Y is a one dimensional numpy array.
+          batch_size is the number of samples in each batch.
+          train is a boolean value indicating training or testing procedure.
+        Returns:
+          loss_value is the average loss function value.
+          acc_value is the prediction accuracy.
+        """
+        n = X.shape[0]
+        slices = list(gen_batches(n, batch_size))
+        num_batch = len(slices)
+        idx = list(range(n))
+        np.random.shuffle(idx)
+        loss_value, acc_value = 0, 0
+        for i, index in enumerate(slices):
+            index = idx[slices[i]]
+            x, y = X[index, :], Y[index]
+            z, yhat = model.forward(x)  # Execute forward pass
+            if train:
+                dLdz = self.loss.derivative(z, y)  # Calculate derivative of the loss with respect to out
+                self.backward(dLdz)  # Execute the backward pass to compute the deltas
+                self.update_weights()  # Calculate the gradients and update the weights
+            loss_value += self.loss.value(z, y) * x.shape[0]
+            acc_value += accuracy(yhat, y) * x.shape[0]
+        loss_value = loss_value / n
+        acc_value = acc_value / n
+        return loss_value, acc_value
+
+
+def train(model: NeuralNetwork, X: np.ndarray, Y: np.ndarray, batch_size: int, epoches: int) -> (
+        List[np.ndarray], List[float]):
+    """
+    trains the neural network.
+    Parameters:
+      model is a NeuralNetwork object.
+      X is the data input. X is a two-dimensional numpy array.
+      Y is the data label. Y is a one dimensional numpy array.
+      batch_size is the number of samples in each batch.
+      epoches is an integer, representing the number of epoches.
+    Returns:
+      epoch_loss is a list of float numbers, representing loss function value in all epoches.
+      epoch_acc is a list of float numbers, representing the accuracies in all epoches.
+    """
+    loss_value, acc = model.one_epoch(X, Y, batch_size, train=False)
+    epoch_loss, epoch_acc = [loss_value], [acc]
+    print('Initialization: ', 'loss %.4f  ' % loss_value, 'accuracy %.2f' % acc)
+    for epoch in range(epoches):
+        if epoch % 100 == 0 and epoch > 0:  # decrease the learning rate
+            model.learning_rate = min(model.learning_rate / 10, 1.0e-5)
+        loss_value, acc = model.one_epoch(X, Y, batch_size, train=True)
+        if epoch % 10 == 0:
+            print("Epoch {}/{}: Loss={}, Accuracy={}".format(epoch, epoches, loss_value, acc))
+        epoch_loss.append(loss_value)
+        epoch_acc.append(acc)
+    return epoch_loss, epoch_acc
+
+
+# training procedure
+num_sample, num_feature = trainX.shape
+epoches = 80
+batch_size = 128
+Loss = []
+Acc = []
+learning_rate = 1 / num_sample * batch_size
+np.random.seed(2024)
+model = NeuralNetwork([784, 1024, 512, 10], [Identity(), ReLU(), ReLU(), Softmax()], CEwithLogit(),
+                      learning_rate=learning_rate)
+epoch_loss, epoch_acc = train(model, trainX, trainY, batch_size, epoches)
+
+# testing procedure
+test_loss, test_acc = model.one_epoch(testX, testY, batch_size, train=False)
+z, yhat = model.forward(testX)
+yhat = np.argmax(yhat, axis=1)
+y = np.argmax(testY, axis=1)
+
+# print(confusion_matrix(yhat, y))
+print(classification_report(yhat, y))
 
